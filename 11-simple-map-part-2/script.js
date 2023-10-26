f866752be872fd7aab6c16722a5b837ea26265cc
Xu Jie Sng <sngxujie@gmail.com>
2023-10-26 03:45:06 +0000

    add zoo marker and ammend bracket error

--- 11-simple-map-part-2/script.js
+++ 11-simple-map-part-2/script.js
@@ -1,14 +1,26 @@
 // load the map object
-const map = L.map('map').setView([1.3521, 103.8198], 12);
+const mapObject = L.map('map').setView([1.3521, 103.8198], 12);
 
 
 // set up the tile layer
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+}).addTo(mapObject);
+
+
+// lets add a simple marker on the zoo
+const marker = L.marker([1.4043, 103.7930]);
+marker.addTo(mapObject);
+
+
+// async function loadCarparkData(){
+//     let response = await axios.get`https://data.gov.sg/collections/{collection_id}/view`
+//     return response.data
+// }
+
 
 // Loads once the DOM is loaded, you can add your APIs below
-document.addEventListener("DOMContentLoaded", async function(){
+// document.addEventListener("DOMContentLoaded", async function(){
 
-})
+// })
