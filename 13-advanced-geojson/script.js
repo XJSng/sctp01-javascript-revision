920aa2b24058ab15e22dcc97ae15bc1396878f68
Xu Jie Sng <sngxujie@gmail.com>
2023-11-01 09:36:17 +0000

    load and test all cycling paths

--- 13-advanced-geojson/script.js
+++ 13-advanced-geojson/script.js
@@ -1,23 +1,33 @@
-document.addEventListener("DOMContentLoaded", async function(){
-const singapore = [1.3521, 103.8198]
+document.addEventListener("DOMContentLoaded", async function () {
+    const singapore = [1.3521, 103.8198]
     const map = L.map('map').setView(singapore, 12);
-  
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
 
-// Add code below
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    // Add code below
     const cycling = await loadJsonData("data/cycling.geojson")
     const cyclingLayer = L.geoJson(cycling, {
-        onEachFeature: function(features, layer) {
+        onEachFeature: function (features, layer) {
             const tempElement = document.createElement("div")
             tempElement.innerHTML = features.properties.Description
-            console.log(tempElement)
+            const allTDs = tempElement.querySelectorAll("td")
+            const cyclingPath = allTDs[0].innerHTML;
+            const agency = allTDs[1].innerHTML;
+            layer.bindPopup(`<h2>${cyclingPath}</h2><p><b>Managed by: </b>${agency}<p>`)
         }
     })
-    // console.log(cyclingLayer)
-})
+    cyclingLayer.setStyle(
+        {
+            color: "red",
+            weight: 4
+        }
+    )
+    cyclingLayer.addTo(map)
+});
+
 
 async function loadJsonData(filePath) {
     const response = await axios.get(filePath)
