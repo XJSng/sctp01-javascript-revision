c3f96fa240c409cd425b9b331d868448af23c87b
Xu Jie Sng <sngxujie@gmail.com>
2023-11-01 09:15:33 +0000

    load cycling paths and test if contents can be loaded on console

--- 13-advanced-geojson/script.js
+++ 13-advanced-geojson/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", async function(){
 const singapore = [1.3521, 103.8198]
     const map = L.map('map').setView(singapore, 12);
   
@@ -6,4 +6,20 @@ const singapore = [1.3521, 103.8198]
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// Add code below
+    const cycling = await loadJsonData("data/cycling.geojson")
+    const cyclingLayer = L.geoJson(cycling, {
+        onEachFeature: function(features, layer) {
+            const tempElement = document.createElement("div")
+            tempElement.innerHTML = features.properties.Description
+            console.log(tempElement)
+        }
+    })
+    // console.log(cyclingLayer)
 })
+
+async function loadJsonData(filePath) {
+    const response = await axios.get(filePath)
+    return response.data
+}
